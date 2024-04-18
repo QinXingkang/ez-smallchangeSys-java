1a6b51e9756641ef9dbb0a81918b0b482f47e725
QinXingkang <114222919+QinXingkang@users.noreply.github.com>
2024-04-18 23:24:15 +0800

    Add files via upload

---
+++ smallChangeSys/SmallChangeSysApp.java
@@ -0,0 +1,12 @@
+package ProjectExercise.smallChangeSys;
+
+/*
+ * @date
+ * @content
+ */
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        SmallChangeSysOOP smallChangeSysOOP = new SmallChangeSysOOP();
+        smallChangeSysOOP.mainMenu();
+    }
+}
---
+++ smallChangeSys/SmallChangeSysOOP.java
@@ -0,0 +1,141 @@
+package ProjectExercise.smallChangeSys;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/*
+ * @date
+ * @content
+ */
+/*
+该类是完成零钱通的各个功能的类
+使用OOP，即面向对象编程
+将各个功能对应一个方法
+
+    //1.先完成显示菜单，并可以选择菜单，给出相应提示
+    //2.完成零钱通明细
+    //3.收益入账
+    //4.消费
+    //5.退出
+ */
+public class SmallChangeSysOOP {
+    //属性
+    //定义相关的变量
+    Scanner scanner = new Scanner(System.in);
+    boolean loop = true;
+    String key = "";
+    //2.零钱通明细
+    //（1）数组
+    //（2）对象
+    //（3）String拼接
+
+    //(3)
+    String details = "==============零钱通明细==============";
+
+
+    //3.完成收益入账
+    double money = 0;
+    double balance = 0;
+    Date date = null;
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");//可以用于日期格式化
+
+    //4.消费
+    //定义新变量，保存消费的原因
+    String note = "";
+    //完成零钱通显示菜单，并可以选择
+    public  void mainMenu(){
+        do{
+            System.out.println("\n==============零钱通菜单==============");
+            System.out.println("\t\t\t1 零钱通明细");
+            System.out.println("\t\t\t2 收益入账");
+            System.out.println("\t\t\t3 消费");
+            System.out.println("\t\t\t4 退出");
+
+            System.out.println("请选择(1-4)：");
+            key = scanner.next();
+
+            //使用switch分支控制
+            switch(key){
+                case "1":
+                    this.detail();
+                    break;
+                case "2":
+                    this.income();
+                    break;
+                case "3":
+                    this.pay();
+                    break;
+                case "4":
+                    this.exit();
+                    break;
+                default:
+                    System.out.println("选择有误，请重新选择");
+            }
+        }while(loop);
+    }
+
+    //完成零钱通明细
+    public void detail(){
+        System.out.println(details);
+    }
+
+    //完成收益入账
+    public void income(){
+        System.out.print("收益入账金额：");
+        money = scanner.nextDouble();
+        //money 的值范围应该校验一下
+        if(money > 0){
+            balance += money;
+        }else{
+            System.out.println("金额输入有误");
+        }
+
+
+        //拼接收益入账信息到details
+        date = new Date();//获取当前日期
+        details += "\n收益入账" + "\t+" + money + "\t" + simpleDateFormat.format(date) + "\t" + balance;
+    }
+
+    public void pay(){
+        System.out.println("消费金额：");
+        money = scanner.nextDouble();
+        //money 的值范围应该校验一下
+        if(money > 0 && money <= balance){
+            balance -= money;
+        }else if(money > 0 && money > balance){
+            System.out.println("余额不足，余额剩余" + balance);
+            return;
+        }else{
+            System.out.println("金额输入有误");
+        }
+        System.out.println("请输入消费说明");
+        note = scanner.next();
+
+        //拼接消费信息到details
+        date = new Date();//获取当前时间
+        details += "\n" + note + "\t-" + money + "\t" + simpleDateFormat.format(date) + "\t" + balance;
+    }
+
+    public void exit(){
+        String choice = "";
+        while(true){
+            System.out.println("确定要退出码？ y/n");
+            choice = scanner.next();
+            if("y".equals(choice) || "n".equals(choice)){
+                break;
+            }else{
+                System.out.println("输入有误，请重新输入");
+            }
+
+//                        if(choice.equals("y")){
+//                            loop = false;
+//                        }else if(choice.equals("n")){
+//                            break;
+//                        }
+        }
+        if(choice.equals("y")){
+            loop = false;
+        }
+    }
+}
